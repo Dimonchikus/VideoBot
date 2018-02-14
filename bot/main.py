eb3ad16f0c9be3e22148949cbb094cca003c8504
dimon <dimonchikakus@gmail.com>
2018-02-14 18:24:58 +0200

    1

--- bot/main.py
+++ bot/main.py
@@ -11,7 +11,6 @@ def handle_start(message):
     user_markup.row('addVideo', 'generateVideo', 'stop')
     bot.send_message(message.from_user.id, 'Hello', reply_markup=user_markup)
 
-
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text == "stop":
@@ -26,7 +25,8 @@ def handle_text(message):
 
     elif (str(message.text).__contains__('https://www.youtube.com')) and bot.Flag:
         bot.download_video(message)
-
+    elif (not str(message.text).__contains__('https://www.youtube.com')) and bot.Flag:
+        bot.no_video(message)
     elif bot.Flag:
         bot.get_video(message)
 
