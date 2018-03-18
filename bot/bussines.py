dca475b8213b6df4221a578be26d6cf0c82aa905
Sergey Gorbenko <sergey13gorbenko@gmail.com>
2018-03-18 12:11:06 +0200

    all

--- bot/bussines.py
+++ bot/bussines.py
@@ -77,12 +77,12 @@ def get_video(message):
 
 def download_video(message):
     global Flag_Add
+    global Flag_Priority
     Bot.send_message(message.from_user.id, "Your video has searched")
     print('downloading...')
     with open('list.files', 'a', encoding='utf8') as f:
         f.write(get_name_video(message.text) + '\n' + str(message.text) + '\n')
-    old_name = (get_name_video(message.text)[:-10])
-    new_name = message.text[32:]
+
     try:
         pytube.YouTube(message.text).streams \
             .filter(file_extension='mp4') \
@@ -94,7 +94,7 @@ def download_video(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True)
     user_markup.row('1', '2', '3', '4', '5')
     Bot.send_message(message.from_user.id, "Set the priority of the video", reply_markup=user_markup)
-
+    Flag_Priority = True
     Flag_Add = False
 
 
@@ -138,7 +138,7 @@ def get_name_video(url):
     html_tree = lxml.html.fromstring(r.text)
     path = ".//title"
     name_video = html_tree.xpath(path)[0]
-    return str(name_video.text_content()).replace(' - YouTube','')
+    return str(name_video.text_content()).replace(' - YouTube', '')
 
 
 def user_checker(user_id):
@@ -166,6 +166,7 @@ def add_new_admin(message, user_id):
     Bot.send_message(message.from_user.id, "The contact has been added to the administrator list")
     Flag_Admin = False
 
+
 def list_video(message):
     list_of_video = ''
     j = int(2)
@@ -179,13 +180,9 @@ def list_video(message):
     Bot.send_message(message.from_user.id, list_of_video)
 
 
-
 def delete_video_choise(message):
     Bot.send_message(message.from_user.id,'Choise video for removing:')
     list_video(message)
-  #  for i in list:
-     #   if i[1] == message.text[1]:
-
 
 
 def admin_checker(message):
--- bot/main.py
+++ bot/main.py
@@ -45,7 +45,7 @@ def handle_text(message):
         elif bussines.Flag_Delete and ((re.match(r'^/[0-9]'),message) or (re.match(r'^/[0-9][0-9]'),message)):
             bussines.delete_video(message)
 
-        elif bussines.Flag_Priority and re.match(r'1', message.text):
+        elif bussines.Flag_Priority and re.match(r'^[1-5]$', message.text):
             print("lol")
         else:
             bussines.other(message)
